89ffbabe6df7ba974e5e2160bdad9e785f5112e9
elghorfizineb <elghorfizineb@gmail.com>
2022-01-15 00:06:49 +0100

    2nd change

--- app/src/main/java/com/example/myfitnessapp/MainActivity.java
+++ app/src/main/java/com/example/myfitnessapp/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.myfitnessapp;
 
+import static com.google.android.material.bottomnavigation.BottomNavigationView.*;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -13,6 +20,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //This line hide
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+
+        BottomNavigationView navigationView = findViewById(R.id.nav_view);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new home_fragment()).commit();
+        navigationView.setSelectedItemId(R.id.navigation_home);
+
+
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch(item.getItemId()){
+                    case R.id.navigation_home:
+                        fragment = new home_fragment();
+                        break;
+                    case R.id.workouts:
+                        fragment = new workouts_fragments();
+                        break;
+                    case R.id.diets:
+                        fragment = new bmi_calculator();
+                        break;
+                    case R.id.navigation_notifications:
+                        fragment = new settings_fragment();
+                        break;
+
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
+
+
+                return true;
+            }
+        });
+
+
 
     }
+
 }
---
+++ app/src/main/java/com/example/myfitnessapp/workouts_fragments.java
@@ -0,0 +1,64 @@
+package com.example.myfitnessapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link workouts_fragments#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class workouts_fragments extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public workouts_fragments() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment workouts_fragments.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static workouts_fragments newInstance(String param1, String param2) {
+        workouts_fragments fragment = new workouts_fragments();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.workouts_fragments, container, false);
+    }
+}
